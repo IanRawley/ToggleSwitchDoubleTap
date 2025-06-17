02485e8ece057f3db661026e5da8e211d2d7c424
Ian Rawley <ian.rawley@silmetra.co.nz>
2025-06-18 10:39:58 +1200

    Add project files.

---
+++ ToggleSwitchDoubleTap/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace ToggleSwitchDoubleTap.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+    public bool Value { get; set; } = false;
+
+    public ObservableCollection<string> Messages { get; } = new();
+
+        
+
+    public MainViewModel()
+    {
+        
+    }
+   
+    
+}
---
+++ ToggleSwitchDoubleTap/Views/MainView.axaml.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Input.TextInput;
+using Avalonia.Interactivity;
+using Avalonia.ReactiveUI;
+using Avalonia.Threading;
+using ToggleSwitchDoubleTap.ViewModels;
+
+namespace ToggleSwitchDoubleTap.Views;
+
+public partial class MainView : ReactiveUserControl<MainViewModel>
+{
+    public MainView()
+    {
+        InitializeComponent();
+        this.AddHandler<KeyEventArgs>(KeyUpEvent, (o, e) =>
+        {
+            
+                ViewModel?.Messages.Insert(0, $"KeyUp Detected: Key -> {e.Key} Modifiers -> {e.KeyModifiers} Handled -> {e.Handled}");
+            
+        }, handledEventsToo: true);
+        this.AddHandler<KeyEventArgs>(KeyDownEvent, (o, e) =>
+        {
+
+            ViewModel?.Messages.Insert(0, $"KeyDown Detected: Key -> {e.Key} Modifiers -> {e.KeyModifiers} Handled -> {e.Handled}");
+
+        }, handledEventsToo: true);
+
+        this.AddHandler<TextInputMethodClientRequestedEventArgs>(TextInputMethodClientRequestedEvent, (o, e) =>
+        {
+            ViewModel?.Messages.Insert(0, $"IME Client Requested: Source -> {e.Source}, Client -> {e.Client}, Handled -> {e.Handled}");
+        }, handledEventsToo: true);
+
+        this.AddHandler<TextInputMethodClientRequeryRequestedEventArgs>(InputMethod.TextInputMethodClientRequeryRequestedEvent, (o, e) =>
+        {
+            ViewModel?.Messages.Insert(0, $"IME Client Requery Requested: Source -> {e.Source}, Handled -> {e.Handled}");
+        }, handledEventsToo: true);
+    }
+
+    
+}
